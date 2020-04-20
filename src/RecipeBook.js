6f7be16973ccb0d28f70a954e575c04a84ccfbcf
Luke Smyth <smythluke123@gmail.com>
2020-04-20 13:48:12 +0100

    Convert RecipeBook to Class Component

--- src/RecipeBook.js
+++ src/RecipeBook.js
@@ -16,44 +16,46 @@ function RecipeCard(props){
 	);
 }
 
-function RecipeBook(props){
-	let recipes = [
-		{
-			title: "Chocolate Chip Cookies",
-			description: "Crisp edges, chewy middles.",
-			rating: 4
-		},
-		{
-			title: "Baked Teriyaki Chicken",
-			description: "A much requested chicken recipe! Easy to double for a large group. Delicious!",
-			rating: 4
-		},
-		{
-			title: "Turkey Bolognese Recipe",
-			description: "This is a lighter Bolognese with all the flavor made with lean ground turkey. Mix with freshly boiled pasta.",
-			rating: 2
-		},
-		{
-			title: "Kale and Banana Smoothie",
-			description: "Nutrient-rich kale is hidden in this delicious banana smoothie. Perfect for those of us who have a hard time getting our daily dose of veggies!",
-			rating: 1
-		},
-		{
-			title: "Pancakes",
-			description: "Tall and fluffy. These pancakes are just right. Topped with strawberries and whipped cream, they are impossible to resist.",
-			rating: 5
-		}
-	];
-
-	return(
-		<section className="container">
-			<div className="row row-cols-1 row-cols-md-3">
-				{recipes.map((recipe, i) => (
-					<RecipeCard key={i} title={recipe.title} description={recipe.description} rating={recipe.rating+"/5"} />
-				))}
-			</div>
-		</section>
-	);
+class RecipeBook extends React.Component {
+	render(){
+		let recipes = [
+			{
+				title: "Chocolate Chip Cookies",
+				description: "Crisp edges, chewy middles.",
+				rating: 4
+			},
+			{
+				title: "Baked Teriyaki Chicken",
+				description: "A much requested chicken recipe! Easy to double for a large group. Delicious!",
+				rating: 4
+			},
+			{
+				title: "Turkey Bolognese Recipe",
+				description: "This is a lighter Bolognese with all the flavor made with lean ground turkey. Mix with freshly boiled pasta.",
+				rating: 2
+			},
+			{
+				title: "Kale and Banana Smoothie",
+				description: "Nutrient-rich kale is hidden in this delicious banana smoothie. Perfect for those of us who have a hard time getting our daily dose of veggies!",
+				rating: 1
+			},
+			{
+				title: "Pancakes",
+				description: "Tall and fluffy. These pancakes are just right. Topped with strawberries and whipped cream, they are impossible to resist.",
+				rating: 5
+			}
+		];
+		
+		return(
+			<section className="container">
+				<div className="row row-cols-1 row-cols-md-3">
+					{recipes.map((recipe, i) => (
+						<RecipeCard key={i} title={recipe.title} description={recipe.description} rating={recipe.rating+"/5"} />
+					))}
+				</div>
+			</section>
+		);
+	}
 }
 
 export default RecipeBook;
